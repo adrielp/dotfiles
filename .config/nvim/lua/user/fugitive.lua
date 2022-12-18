37f7a50c3117e2707fee32a00f8eefe61533cd2d
Adriel Perkins <adriel@adrielperkins.dev>
2022-12-18 16:46:35 -0500

    feat: add vim fugitive autocommands

--- .config/nvim/lua/user/fugitive.lua
+++ .config/nvim/lua/user/fugitive.lua
@@ -4,31 +4,35 @@ vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 -- to write my own. Would definitely like to leverage this time of function
 -- particularly when doing commits, pushing, interactive rebasing, and ammending
 -- without editing
---
--- local ThePrimeagen_Fugitive = vim.api.nvim_create_augroup("ThePrimeagen_Fugitive", {})
--- 
--- local autocmd = vim.api.nvim_create_autocmd
--- autocmd("BufWinEnter", {
---     group = ThePrimeagen_Fugitive,
---     pattern = "*",
---     callback = function()
---         if vim.bo.ft ~= "fugitive" then
---             return
---         end
--- 
---         local bufnr = vim.api.nvim_get_current_buf()
---         local opts = {buffer = bufnr, remap = false}
---         vim.keymap.set("n", "<leader>p", function()
---             vim.cmd.Git('push')
---         end, opts)
--- 
---         -- rebase always
---         vim.keymap.set("n", "<leader>P", function()
---             vim.cmd.Git({'pull',  '--rebase'})
---         end, opts)
--- 
---         -- NOTE: It allows me to easily set the branch i am pushing and any tracking
---         -- needed if i did not set the branch up correctly
---         vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts);
---     end,
--- })
+
+local auto_fugs = vim.api.nvim_create_augroup("auto_figs", {})
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd("BufWinEnter", {
+    group = auto_fugs,
+    pattern = "*",
+    callback = function()
+        if vim.bo.ft ~= "fugitive" then
+            return
+        end
+
+        local bufnr = vim.api.nvim_get_current_buf()
+        local opts = {buffer = bufnr, remap = false}
+        vim.keymap.set("n", "<leader>p", function()
+            vim.cmd.Git('push')
+        end, opts)
+
+        -- rebase always
+        vim.keymap.set("n", "<leader>P", function()
+            vim.cmd.Git({'pull',  '--rebase'})
+        end, opts)
+
+        vim.keymap.set("n", "<leader>gc", function()
+            vim.cmd.Git({'commit'})
+        end, opts)
+
+        vim.keymap.set("n", "<leader>gca", ":G commit --amend --no-edit", opts)
+        vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts)
+
+    end,
+})
