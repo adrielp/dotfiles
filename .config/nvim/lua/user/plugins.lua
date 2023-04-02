f5f82edb21183abcdf406d3385ede0a92d9377a2
Adriel Perkins <adriel@adrielperkins.dev>
2023-04-01 23:05:59 -0400

    chore: disable chatgpt plugin for now

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -42,19 +42,20 @@ return require('packer').startup(function(use)
 
   -- Optionals
   use("github/copilot.vim")
-  use({
-    "jackMort/ChatGPT.nvim",
-      config = function()
-        require("chatgpt").setup({
-          -- optional configuration
-        })
-      end,
-      requires = {
-        "MunifTanjim/nui.nvim",
-        "nvim-lua/plenary.nvim",
-        "nvim-telescope/telescope.nvim"
-      }
-  })
+  -- use({
+  --   "jackMort/ChatGPT.nvim",
+  --     config = function()
+  --       require("chatgpt").setup({
+  --         -- optional configuration
+  --       })
+  --     end,
+  --     requires = {
+  --       "MunifTanjim/nui.nvim",
+  --       "nvim-lua/plenary.nvim",
+  --       "nvim-telescope/telescope.nvim"
+  --     }
+  -- })
+
   use({
     'rose-pine/neovim',
     as = 'rose-pine',
