be82a78895e08128ba084c5e3e05d7573324c1df
Adriel Perkins <adriel@adrielperkins.com>
2025-05-11 11:07:48 -0400

    fix: remove runes and crosses from default cosmic lualine example

--- .config/nvim/lua/core/lualine.lua
+++ .config/nvim/lua/core/lualine.lua
@@ -195,29 +195,8 @@ math.randomseed(os.time())
 -- Icon sets for random selection
 local icon_sets = {
   stars = { '★', '☆', '✧', '✦', '✶', '✷', '✸', '✹' }, -- Set of star-like icons
-  runes = {
-    '✠',
-    '⛧',
-    '𖤐',
-    'ᛟ',
-    'ᚨ',
-    'ᚱ',
-    'ᚷ',
-    'ᚠ',
-    'ᛉ',
-    'ᛊ',
-    'ᛏ',
-    '☠',
-    '☾',
-    '♰',
-    '✟',
-    '☽',
-    '⚚',
-    '🜏',
-  }, -- Set of rune-like symbols
   hearts = { '❤', '♥', '♡', '❦', '❧' }, -- Set of heart-shaped icons
   waves = { '≈', '∿', '≋', '≀', '⌀', '≣', '⌇' }, -- Set of wave-like symbols
-  crosses = { '☨', '✟', '♰', '♱', '⛨', '' }, -- Set of cross-like symbols
 }
 
 -- Function to select a random icon from a given set
